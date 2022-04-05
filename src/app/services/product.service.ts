74982d2bb1fb97d9efdc710dd68ee79d7ac73aba
ArPal <aritrarpal@gmail.com>
2022-04-05 16:32:35 +0530

    Showing up products in a table

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Product } from '../common/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'http://localhost:8010/api/productEntities';
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.productEntities)
+    );
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    productEntities: Product[];
+  }
+}
