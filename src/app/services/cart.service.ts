e0a1e8e6bf1bca20012a9af61039e06b1f7151b0
ArPal <aritrarpal@gmail.com>
2022-04-20 19:37:32 +0530

    Implemmented shopping cart increment decrement feature

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -58,4 +58,21 @@ export class CartService {
     console.log(`TotalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
     console.log(`--------------------`);
   }
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+    if(theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    } else {
+      this.computeCartTotals();
+    }
+  }
+  remove(theCartItem: CartItem) {
+    const itemIndex = this.cartItems.findIndex(
+      thisCartItem => thisCartItem.id == theCartItem.id
+    );
+    if(itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+      this.computeCartTotals();
+    }
+  }
 }
